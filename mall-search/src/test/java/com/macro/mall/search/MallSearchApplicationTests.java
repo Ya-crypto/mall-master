88eed9eff35bd8b793f68bccc250b7cfa5c8a2f7
yangren@baosight.com <Deariloveyou1234@>
2022-05-28 18:24:08 +0800

    es基础操作

--- mall-search/src/test/java/com/macro/mall/search/MallSearchApplicationTests.java
+++ mall-search/src/test/java/com/macro/mall/search/MallSearchApplicationTests.java
@@ -1,19 +1,21 @@
 package com.macro.mall.search;
 
 import org.apache.http.HttpHost;
-import org.elasticsearch.action.update.UpdateRequest;
-import org.elasticsearch.action.update.UpdateResponse;
+import org.elasticsearch.action.search.SearchRequest;
+import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
-import org.elasticsearch.common.xcontent.XContentType;
+import org.elasticsearch.index.query.*;
+import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.SearchHits;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.io.IOException;
-import java.util.Collections;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -39,7 +41,7 @@ public class MallSearchApplicationTests {
 //    }
 
     @Test
-    public void test() throws IOException {
+    public void testBaseSE() throws IOException {
         //创建es客户端
         RestHighLevelClient esClinent =  new RestHighLevelClient(
                 RestClient.builder(new HttpHost("localhost",9200,"http")));
@@ -73,11 +75,146 @@ public class MallSearchApplicationTests {
 //        System.out.println(response.getResult());
 
         //修改数据
-        UpdateRequest request = new UpdateRequest();
-        request.index("user").id("1001");
-        request.doc(XContentType.JSON,"age",30);
-        UpdateResponse response = esClinent.update(request, RequestOptions.DEFAULT);
-        System.out.println(response.getResult());
+//        UpdateRequest request = new UpdateRequest();
+//        request.index("user").id("1001");
+//        request.doc(XContentType.JSON,"age",30);
+//        UpdateResponse response = esClinent.update(request, RequestOptions.DEFAULT);
+//        System.out.println(response.getResult());
+
+        //查询数据
+//        GetRequest request = new GetRequest();
+//        request.index("user").id("1001");
+//        GetResponse response = esClinent.get(request, RequestOptions.DEFAULT);
+//        System.out.println(response.getSourceAsString());
+
+        //删除数据
+//        DeleteRequest request = new DeleteRequest();
+//        request.index("user").id("1001");
+//        DeleteResponse response = esClinent.delete(request, RequestOptions.DEFAULT);
+//        System.out.println(response.getResult());
+
+        //批量插入数据
+//        BulkRequest request = new BulkRequest();
+//        for(int i=0;i<100000;i++){
+//            EsUser user = new EsUser("zhangsan" + i, "admin" + i, i, "测试" + i);
+//            String json = new ObjectMapper().writeValueAsString(user);
+//            request.add(new IndexRequest().index("user").id(UUID.randomUUID().toString()).source(json,XContentType.JSON));
+//        }
+//        request.add(new IndexRequest().index("user").id("4001").source(XContentType.JSON,"name","lisi","age",25,"sex","男"));
+//        request.add(new IndexRequest().index("user").id("4002").source(XContentType.JSON,"name","wangwu","age",15,"sex","男"));
+//        request.add(new IndexRequest().index("user").id("4003").source(XContentType.JSON,"name","zhaoliu","age",35,"sex","女"));
+//        request.add(new IndexRequest().index("user").id("4004").source(XContentType.JSON,"name","zhaoliu1","age",35,"sex","女"));
+//        request.add(new IndexRequest().index("user").id("4005").source(XContentType.JSON,"name","zhaoliu2","age",35,"sex","男"));
+//        request.add(new IndexRequest().index("user").id("4006").source(XContentType.JSON,"name","zhaoliu3","age",35,"sex","女"));
+//        BulkResponse response = esClinent.bulk(request, RequestOptions.DEFAULT);
+//        System.out.println(response.getTook());//执行时间
+        //System.out.println(response.getItems());
+
+        //批量删除
+//        BulkRequest request = new BulkRequest();
+//        request.add(new DeleteRequest().index("user").id("2001"));
+//        request.add(new DeleteRequest().index("user").id("2002"));
+//        request.add(new DeleteRequest().index("user").id("2003"));
+//        BulkResponse response = esClinent.bulk(request, RequestOptions.DEFAULT);
+//        System.out.println(response.getTook());//执行时间
+//        System.out.println(response.getItems());
+
+        //全量查询
+//        SearchRequest request = new SearchRequest();
+//        request.indices("user");
+//        request.source(new SearchSourceBuilder().query(QueryBuilders.matchAllQuery()));
+//        SearchResponse response = esClinent.search(request, RequestOptions.DEFAULT);
+//        SearchHits hits = response.getHits();
+//        System.out.println(hits.getTotalHits());
+//        for(SearchHit hit : hits){
+//            System.out.println(hit.getSourceAsString());
+//        }
+        //条件查询
+//        SearchRequest request = new SearchRequest();
+//        request.indices("user");
+//        request.source(new SearchSourceBuilder().query(QueryBuilders.termQuery("name","lisi")));
+//        SearchResponse response = esClinent.search(request, RequestOptions.DEFAULT);
+//        SearchHits hits = response.getHits();
+//        System.out.println(hits.getTotalHits());
+//        for(SearchHit hit : hits){
+//            System.out.println(hit.getSourceAsString());
+//        }
+        //分页查询
+//        SearchRequest request = new SearchRequest();
+//        request.indices("user");
+//        SearchSourceBuilder sourceBuilder = new SearchSourceBuilder().query(QueryBuilders.termQuery("sex","女"));
+//        sourceBuilder.sort("age", SortOrder.ASC);
+//        sourceBuilder.from(0);
+//        sourceBuilder.size(2);
+//        String[] excludes = {"age"};
+//        String[] includes= {};
+//        sourceBuilder.fetchSource(includes,excludes);
+//        request.source(sourceBuilder);
+//
+//        SearchResponse response = esClinent.search(request, RequestOptions.DEFAULT);
+//        SearchHits hits = response.getHits();
+//        System.out.println(hits.getTotalHits());
+//        for(SearchHit hit : hits){
+//            System.out.println(hit.getSourceAsString());
+//        }
+
+       //组合查询
+//        SearchRequest request = new SearchRequest();
+//        request.indices("user");
+//        SearchSourceBuilder sourceBuilder = new SearchSourceBuilder();
+//        BoolQueryBuilder boolQueryBuilder = QueryBuilders.boolQuery();
+//        boolQueryBuilder.must(QueryBuilders.matchQuery("sex","男"));
+//        //boolQueryBuilder.must(QueryBuilders.matchQuery("age",15));
+//        //boolQueryBuilder.should(QueryBuilders.matchQuery("age",35));或者可以是35
+//        RangeQueryBuilder rangeQueryBuilder = QueryBuilders.rangeQuery("age");
+//        rangeQueryBuilder.gte(15);
+//        rangeQueryBuilder.lte(35);
+//        sourceBuilder.query(boolQueryBuilder);
+//        sourceBuilder.query(rangeQueryBuilder);
+//        request.source(sourceBuilder);
+//        SearchResponse response = esClinent.search(request, RequestOptions.DEFAULT);
+//        System.out.println(response.getTook());
+//        SearchHits hits = response.getHits();
+//        for(SearchHit hit:hits){
+//            System.out.println(hit.getSourceAsString());
+//        }
+
+        //范围查询
+        SearchRequest request = new SearchRequest();
+        request.indices("user");
+        SearchSourceBuilder sourceBuilder = new SearchSourceBuilder();
+        RangeQueryBuilder rangeQueryBuilder = QueryBuilders.rangeQuery("age");
+        rangeQueryBuilder.gte(15);
+        //rangeQueryBuilder.lte(35);
+        sourceBuilder.query(rangeQueryBuilder);
+        request.source(sourceBuilder);
+        SearchResponse response = esClinent.search(request, RequestOptions.DEFAULT);
+        System.out.println(response.getTook());
+        SearchHits hits = response.getHits();
+        for(SearchHit hit:hits){
+            System.out.println(hit.getSourceAsString());
+        }
+
+        //模糊查询
+//        SearchRequest request = new SearchRequest();
+//        request.indices("user");
+//        SearchSourceBuilder sourceBuilder = new SearchSourceBuilder();
+//        //FuzzyQueryBuilder queryBuilder = QueryBuilders.fuzzyQuery("name", "zhaoliu").fuzziness(Fuzziness.ONE);
+//        HighlightBuilder highlightBuilder = new HighlightBuilder();
+//        highlightBuilder.preTags("<font style='color:red'>");
+//        highlightBuilder.postTags("</font>");
+//        highlightBuilder.field("name");
+//        sourceBuilder.highlighter(highlightBuilder);
+//        sourceBuilder.query(QueryBuilders.matchQuery("name", "zhaoliu"));
+//        request.source(sourceBuilder);
+//        SearchResponse response = esClinent.search(request, RequestOptions.DEFAULT);
+//        System.out.println(response.getTook());
+//        System.out.println(response.toString());
+//        SearchHits hits = response.getHits();
+//        for(SearchHit hit:hits){
+//            System.out.println(hit.getSourceAsString());
+//        }
+
         //关闭客户端
         esClinent.close();
     }
